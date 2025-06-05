6770178f36f2613e310ffded7e943fbffb7f660b
Brian Lynnerup Pedersen <brian.lynnerup.pedersen@gmail.com>
2025-06-05 20:47:26 +0200

    Something about narrowing

--- generics/generics-advanced-index.ts
+++ generics/generics-advanced-index.ts
@@ -2,35 +2,60 @@
 // This file demonstrates advanced usage of generics in TypeScript.
 // The return type for the function is inferred based on the input types, and thus optional.
 function merge<T, U>(obj1: T, obj2: U) {
-    return { ...obj1, ...obj2 };
+  return { ...obj1, ...obj2 };
 }
 
 // In this function the return type is explicitly defined as a combination of the two input types.
 // This is useful when you want to ensure that the return type is a combination of both input types.
 function merge2<T, U>(obj1: T, obj2: U): T & U {
-    return { ...obj1, ...obj2 };
+  return { ...obj1, ...obj2 };
 }
 
 // Merges the two objects and returns a new object which contains properties from both.
-console.log(merge({ name: 'Alice' }, { age: 30 })); // { name: 'Alice', age: 30 }
+console.log(merge({ name: "Alice" }, { age: 30 })); // { name: 'Alice', age: 30 }
 // Note how the property name is overwritten if it exists in both objects
-console.log(merge({ name: 'Bob' }, {name: 'Bobson', living: 'poorly'})); 
+console.log(merge({ name: "Bob" }, { name: "Bobson", living: "poorly" }));
 
 // You can limit the types of the objects that can be passed to the function using constraints.
-function mergeWithConstraint<T extends object, U extends object>(obj1: T, obj2: U): T & U {
-    return { ...obj1, ...obj2 };
+function mergeWithConstraint<T extends object, U extends object>(
+  obj1: T,
+  obj2: U
+): T & U {
+  return { ...obj1, ...obj2 };
 }
 
 function mergeCatWithPlace<T extends Cat, U extends string>(cat: T, place: U) {
-    return { ...cat, place };
+  return { ...cat, place };
 }
 
 interface Cat {
-    name: string
-    age: number
+  name: string;
+  age: number;
 }
 
 console.log("Constraint merge");
-console.log(mergeWithConstraint({ name: 'Charlie' }, { age: 5 })); // { name: 'Charlie', age: 5 }
+console.log(mergeWithConstraint({ name: "Charlie" }, { age: 5 })); // { name: 'Charlie', age: 5 }
 console.log("Cat");
-console.log(mergeCatWithPlace({ name: 'Whiskers', age: 3 }, 'Garden')); // { name: 'Whiskers', age: 3, place: 'Garden' }
+console.log(mergeCatWithPlace({ name: "Whiskers", age: 3 }, "Garden")); // { name: 'Whiskers', age: 3, place: 'Garden' }
+
+// Narrowing using the 'in' operator
+function getRuntime(media: Movie | TVShow): number {
+  // Check if the 'duration' property exists in the media object
+  // If it does, it's a Movie; otherwise, it's a TVShow
+  if ("duration" in media) {
+    return media.duration; // Movie
+  } else {
+    return media.episodes * media.episodeDuration; // TVShow
+  }
+}
+
+interface Movie {
+  title: string;
+  duration: number;
+}
+
+interface TVShow {
+  title: string;
+  episodes: number;
+  episodeDuration: number;
+}
